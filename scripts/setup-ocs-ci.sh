0f7085ba7205c976d598786176e6e07dfff1c402
Shilpi Das <69339560+Shilpi-Das1@users.noreply.github.com>
2026-06-16 11:02:35 +0530

    Use dynamic Python binary for virtual environment

--- scripts/setup-ocs-ci.sh
+++ scripts/setup-ocs-ci.sh
@@ -129,7 +129,8 @@ fi
 
 rm -rf "$WORKSPACE/venv"
 
-/usr/local/bin/python3.11 -m venv "$WORKSPACE/venv"
+PYTHON_BIN=$(command -v python3.11 || command -v python3)
+"$PYTHON_BIN" -m venv "$WORKSPACE/venv"
 
 . "$WORKSPACE/venv/bin/activate"		# activate named python venv
 
